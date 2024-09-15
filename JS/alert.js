9281a68b3da822e526b3321164cdbe641ebbf801
Nikitich Oleksandr <sashanikitich@gmail.com>
2024-09-15 13:02:48 +0300

    validation for alert

--- JS/alert.js
+++ JS/alert.js
@@ -2,23 +2,34 @@ var close = document.getElementsByClassName("close-btn");
 var i;
 
 function open_alert() {
-    // Assuming there's only one alert, you can target the first element
     var alert = document.getElementsByClassName("alert")[0];
     alert.style.display = "block";
+    alert.style.opacity = "1";
+
+    setTimeout(function() {
+        alert.style.opacity = "0";
+        setTimeout(function() { alert.style.display = "none"; }, 600);
+    }, 3000);
 }
 
-// Loop through all close buttons
+function validateForm() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var question = document.getElementById("question").value;
+
+    if (name === "" || email === "" || question === "") {
+        alert("Please fill in all fields.");
+        return false;  // Prevent form submission
+    } else {
+        open_alert();  // Show the alert if the form is valid
+        return true;   // Allow form submission
+    }
+}
+
 for (i = 0; i < close.length; i++) {
-    // When someone clicks on a close button
     close[i].onclick = function(){
-
-        // Get the parent of <span class="close-btn"> (<div class="alert">)
         var div = this.parentElement;
-
-        // Set the opacity of div to 0 (transparent)
         div.style.opacity = "0";
-
-        // Hide the div after 600ms (the same amount of milliseconds it takes to fade out)
         setTimeout(function(){ div.style.display = "none"; }, 600);
     }
 }
